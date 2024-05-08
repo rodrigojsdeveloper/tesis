762418af4fd1a2719b4075b86f60e7e5c0b02e78
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-05-07 23:43:52 -0300

    feat: add phone mask to input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import InputMask from 'react-input-mask'
+
 interface InputProps {
   label: string
   type: string
@@ -7,9 +10,17 @@ interface InputProps {
 }
 
 export const Input = ({ label, type, name, value, onChange }: InputProps) => {
-  return (
-    <div className="flex flex-col justify-between gap-1.5 sm:flex-row sm:items-center">
-      <label className="text-lg font-medium text-blue-200">{label}:</label>
+  const inputField =
+    type === 'tel' ? (
+      <InputMask
+        mask="(99) 99999-9999"
+        value={value}
+        onChange={onChange}
+        className="h-11 w-full max-w-[17rem] rounded-xl border border-solid border-gray-300 bg-slate-50 px-4 py-2"
+      >
+        <input type="tel" name={name} required />
+      </InputMask>
+    ) : (
       <input
         required
         type={type}
@@ -18,6 +29,12 @@ export const Input = ({ label, type, name, value, onChange }: InputProps) => {
         onChange={onChange}
         className="h-11 w-full max-w-[17rem] rounded-xl border border-solid border-gray-300 bg-slate-50 px-4 py-2"
       />
+    )
+
+  return (
+    <div className="flex flex-col justify-between gap-1.5 sm:flex-row sm:items-center">
+      <label className="text-lg font-medium text-blue-200">{label}:</label>
+      {inputField}
     </div>
   )
 }
